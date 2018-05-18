2b0e49039272fa1e7380d41d0c987ccbcfbe8bdb
David Davila <fdaviddavila@gmail.com>
2018-05-18 18:44:34 +0200

    Proposed solution for the project
    + Parsing the JSON is done manually
    + The DetailActivity displays the retrieved data in a quite good way, with some default texts and scrolls when needed
    + Has a landscape layout mode for the DetailedActivity
    + The functions added have JavaDoc style comments
    
    * The parameter for the helper methods used to generate the texts for the TextViews is a Sandwich object. This is done this way in order to avoid passing the context when calling them, or to specify a context when calling the constructor, as they use strings stored in the strings.xml.

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,14 +3,20 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.udacity.sandwichclub.model.Sandwich;
 import com.udacity.sandwichclub.utils.JsonUtils;
 
+import java.util.List;
+
 public class DetailActivity extends AppCompatActivity {
+
+    private static final String TAG = DetailActivity.class.getSimpleName();
 
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
@@ -43,7 +49,7 @@ public class DetailActivity extends AppCompatActivity {
             return;
         }
 
-        populateUI();
+        populateUI(sandwich);
         Picasso.with(this)
                 .load(sandwich.getImage())
                 .into(ingredientsIv);
@@ -56,7 +62,98 @@ public class DetailActivity extends AppCompatActivity {
         Toast.makeText(this, R.string.detail_error_message, Toast.LENGTH_SHORT).show();
     }
 
-    private void populateUI() {
+    private void populateUI(Sandwich sandwich) {
+        TextView placeOfOriginTextView = findViewById(R.id.origin_tv);
+        TextView descriptionTextView = findViewById(R.id.description_tv);
+        TextView alsoKnownAsTextView = findViewById(R.id.also_known_tv);
+        TextView ingredientsTextView = findViewById(R.id.ingredients_tv);
 
+        placeOfOriginTextView.setText(getPlaceOfOrigin(sandwich));
+        descriptionTextView.setText(getDescription(sandwich));
+        alsoKnownAsTextView.setText(getAliases(sandwich));
+        ingredientsTextView.setText(getIngredients(sandwich));
+    }
+
+
+    /**
+     * Creates a String containing the place of origin (or a default text if unavailable or unknown)
+     * from a Sandwich object
+     *
+     * @param sandwich The Sandwich object
+     * @return String containing the origin
+     */
+    private String getPlaceOfOrigin(Sandwich sandwich) {
+        String origin = sandwich.getPlaceOfOrigin();
+        if ((origin == null) || (origin.contentEquals(""))) {
+            origin = getString(R.string.detail_place_of_origin_default);
+        }
+        return origin;
+    }
+
+    /**
+     * Creates a String containing all the known aliases (or a default text if unavailable or unknown)
+     * from a Sandwich object
+     *
+     * @param sandwich The Sandwich object
+     * @return String containing all the known aliases
+     */
+    private String getAliases(Sandwich sandwich) {
+        List<String> stringList = sandwich.getAlsoKnownAs();
+        String alias;
+        if ((stringList == null) || (stringList.size() == 0)) {
+            alias = getString(R.string.detail_also_known_as_default);
+        } else {
+            alias =
+                    getStringFromStringList(stringList,
+                            getString(R.string.string_list_separator_also_known_as));
+        }
+        return alias;
+    }
+
+    /**
+     * Creates a String containing the description (or a default text if unavailable or unknown)
+     * from a Sandwich object
+     *
+     * @param sandwich The Sandwich object
+     * @return String containing the description
+     */
+    private String getDescription(Sandwich sandwich) {
+        String description = sandwich.getDescription();
+        if ((description == null) || (description.contentEquals(""))) {
+            description = getString(R.string.detail_description_default);
+        }
+        return description;
+    }
+
+    /**
+     * Creates a String containing the list of ingredients from a Sandwich object
+     *
+     * @param sandwich The Sandwich object
+     * @return String containing the ingredients list comma-separated
+     */
+    private String getIngredients(Sandwich sandwich) {
+        String ingredients;
+        List<String> stringList = sandwich.getIngredients();
+
+        if ((stringList == null) || (stringList.size() == 0)) {
+            ingredients = getString(R.string.detail_ingredients_default);
+        } else {
+            ingredients =
+                    getStringFromStringList(stringList,
+                            getString(R.string.string_list_separator_ingredients));
+        }
+        return ingredients;
+    }
+
+    /**
+     * Helper method that creates a String by concatenating the contents of a List of Strings,
+     * separating the members with the String provided to this means
+     *
+     * @param stringList List of Strings
+     * @param separator String to separate the strings in the stringList
+     * @return String built with the content of the List of String separated with the specified separator
+     */
+    private String getStringFromStringList(List<String> stringList, String separator) {
+        return TextUtils.join(separator, stringList);
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -1,10 +1,153 @@
 package com.udacity.sandwichclub.utils;
 
+import android.util.Log;
+
 import com.udacity.sandwichclub.model.Sandwich;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class JsonUtils {
 
+    private final static String TAG = JsonUtils.class.getSimpleName();
+
+    // Strings used in the JSON structure for Sandwich
+    private final static String JSON_SANDWICH_NAME = "name";
+    private final static String JSON_SANDWICH_NAME_MAIN_NAME = "mainName";
+    private final static String JSON_SANDWICH_NAME_AKA = "alsoKnownAs";
+    private final static String JSON_SANDWICH_PLACE_OF_ORIGIN = "placeOfOrigin";
+    private final static String JSON_SANDWICH_DESCRIPTION = "description";
+    private final static String JSON_SANDWICH_IMAGE = "image";
+    private final static String JSON_SANDWICH_INGREDIENTS = "ingredients";
+
+
+    /**
+     * Parses a String containing a JSON formatted Sandwich structure.
+     * <p>
+     * The JSON format for a Sandwich is the following:
+     * {
+     * "name": {
+     * "mainName" : String,
+     * "alsoKnownAs" : String[]
+     * },
+     * "placeOfOrigin" : String,
+     * "description" : String,
+     * "image": String,
+     * "ingredients": String[]
+     * }
+     *
+     * @param json The JSON String to be parsed
+     * @return A Sandwich object created using the JSON data (or null if something failed)
+     */
+
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        try {
+            // Create the base JSONObject:
+            JSONObject jsonSandwich = new JSONObject(json);
+
+            // Get all the object's members:
+            String mainName = getMainName(jsonSandwich);
+            List<String> alsoKnownAs = getAlsoKnownAs(jsonSandwich);
+            String placeOfOrigin = getPlaceOfOrigin(jsonSandwich);
+            String description = getDescription(jsonSandwich);
+            String image = getImage(jsonSandwich);
+            List<String> ingredients = getIngredients(jsonSandwich);
+
+            // Create the Sandwich object and return it:
+            return new Sandwich(mainName, alsoKnownAs, placeOfOrigin, description, image, ingredients);
+
+        } catch (JSONException e) {
+            Log.e(TAG, "Parsing the JSON Sandwich structure failed: " + e.getMessage());
+            return null;
+        }
+    }
+
+    /**
+     * Gets the "mainName" value from the JSON Sandwich structure
+     *
+     * @param jsonSandwich the root JSON object
+     * @return A String containing the value of "mainName"
+     * @throws JSONException if failed getting the String from the JSONObject
+     */
+    private static String getMainName(JSONObject jsonSandwich) throws JSONException {
+        JSONObject jsonName = jsonSandwich.getJSONObject(JSON_SANDWICH_NAME);
+        return jsonName.getString(JSON_SANDWICH_NAME_MAIN_NAME);
+    }
+
+    /**
+     * Gets the "alsoKnownAs" values from the JSON Sandwich structure
+     *
+     * @param jsonSandwich the root JSON object
+     * @return A String List containing all the values of "alsoKnownAs"
+     * @throws JSONException if failed creating the JSONObject or the JSONArray
+     */
+    private static List<String> getAlsoKnownAs(JSONObject jsonSandwich) throws JSONException {
+        JSONObject jsonName = jsonSandwich.getJSONObject(JSON_SANDWICH_NAME);
+        JSONArray jsonArray = jsonName.getJSONArray(JSON_SANDWICH_NAME_AKA);
+        return getStringListFromJsonArray(jsonArray);
+    }
+
+    /**
+     * Gets the "placeOfOrigin" value from the JSON Sandwich structure
+     *
+     * @param jsonSandwich the root JSON object
+     * @return A String containing the value of "placeOfOrigin"
+     * @throws JSONException if failed getting the String from the JSONObject
+     */
+    private static String getPlaceOfOrigin(JSONObject jsonSandwich) throws JSONException {
+        return jsonSandwich.getString(JSON_SANDWICH_PLACE_OF_ORIGIN);
+    }
+
+    /**
+     * Gets the "description" value from the JSON Sandwich structure
+     *
+     * @param jsonSandwich the root JSON object
+     * @return A String containing the value of "description"
+     * @throws JSONException if failed getting the String from the JSONObject
+     */
+    private static String getDescription(JSONObject jsonSandwich) throws JSONException {
+        return jsonSandwich.getString(JSON_SANDWICH_DESCRIPTION);
+    }
+
+    /**
+     * Gets the "image" value -the path to an image- from the JSON Sandwich structure
+     *
+     * @param jsonSandwich the root JSON object
+     * @return A String containing the value of "image"
+     * @throws JSONException if failed getting the String from the JSONObject
+     */
+    private static String getImage(JSONObject jsonSandwich) throws JSONException {
+        return jsonSandwich.getString(JSON_SANDWICH_IMAGE);
+    }
+
+    /**
+     * Gets the "ingredients" values from the JSON Sandwich structure
+     *
+     * @param jsonSandwich the root JSON object
+     * @return A String List containing all the values of "ingredients"
+     * @throws JSONException if failed creating the JSONArray
+     */
+    private static List<String> getIngredients(JSONObject jsonSandwich) throws JSONException {
+        JSONArray jsonArray = jsonSandwich.getJSONArray(JSON_SANDWICH_INGREDIENTS);
+        return getStringListFromJsonArray(jsonArray);
+    }
+
+    /**
+     * Returns a List of Strings created from the members of a JSONArray
+     *
+     * @param jsonArray the original JSONArray
+     * @return A List fo Strings with the values of retrieved from the JSONArray
+     * @throws JSONException if failed getting the Strings from the JSONArray
+     */
+    private static List<String> getStringListFromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<String> stringList = new ArrayList<>();
+        for (int item = 0; item < jsonArray.length(); item++) {
+            stringList.add(jsonArray.getString(item));
+        }
+        return stringList;
     }
 }
